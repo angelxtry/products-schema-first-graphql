f42a57f06c0ad560d6932ab0640844f4e2c061be
sean <sean@gomicorp.com>
2022-03-16 13:43:59 +0900

    ProductGroup 추가
    productGroups 데이터 추가.
    ProductGroup type 추가.
    productGroups Query 추가.
    productGroups resolver 추가.

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 import { ApolloServer, gql } from 'apollo-server';
 
+const productGroups = [
+  {
+    id: 'aaaa',
+    productGroupName: '헤라 블랙쿠션',
+  },
+  {
+    id: 'bbbb',
+    productGroupName: '헤라 세럼',
+  },
+];
+
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
       hello: String!
+      productGroups: [ProductGroup!]!
+    }
+    type ProductGroup {
+      id: ID!
+      productGroupName: String!
     }
   `,
   resolvers: {
     Query: {
       hello: () => 'world!',
+      productGroups: () => productGroups,
     },
   },
 });
