57ed14e4c9bfcac704d09533435fb98421b33948
sean <sean@gomicorp.com>
2022-03-16 14:25:57 +0900

    ProductOption 추가

--- index.js
+++ index.js
@@ -34,6 +34,54 @@ const productOptionGroups = [
   },
 ];
 
+const productOptions = [
+  {
+    id: 'po1',
+    productOptionGroupId: 'pog3',
+    productOption: '21 (라이트 베이지)',
+  },
+  {
+    id: 'po2',
+    productOptionGroupId: 'pog3',
+    productOption: '23 (미디움 베이지)',
+  },
+  {
+    id: 'po3',
+    productOptionGroupId: 'pog4',
+    productOption: '12g',
+  },
+  {
+    id: 'po4',
+    productOptionGroupId: 'pog4',
+    productOption: '15g',
+  },
+  {
+    id: 'po5',
+    productOptionGroupId: 'pog1',
+    productOption: '24K 골드 리페어',
+  },
+  {
+    id: 'po6',
+    productOptionGroupId: 'pog1',
+    productOption: '비타민 C 브라이트',
+  },
+  {
+    id: 'po7',
+    productOptionGroupId: 'pog1',
+    productOption: '콜라겐 리프팅',
+  },
+  {
+    id: 'po8',
+    productOptionGroupId: 'pog2',
+    productOption: '35ml',
+  },
+  {
+    id: 'po9',
+    productOptionGroupId: 'pog2',
+    productOption: '55ml',
+  },
+];
+
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
@@ -42,6 +90,8 @@ const server = new ApolloServer({
       productGroup(id: ID!): ProductGroup
       productOptionGroups: [ProductOptionGroup!]!
       productOptionGroup(id: ID!): ProductOptionGroup
+      productOptions: [ProductOption!]!
+      productOption(id: ID!): ProductOption
     }
     type ProductGroup {
       id: ID!
@@ -51,6 +101,11 @@ const server = new ApolloServer({
       id: ID!
       productOptionGroupName: String!
       productGroup: ProductGroup!
+    }
+    type ProductOption {
+      id: ID!
+      productOption: String!
+      productOptionGroup: ProductOptionGroup!
     }
   `,
   resolvers: {
@@ -66,11 +121,25 @@ const server = new ApolloServer({
           (productOptionGroup) => productOptionGroup.id === productOptionGroupId,
         );
       },
+      productOptions: () => productOptions,
+      productOption: (parent, { id: productOptionId }, context) => {
+        return productOptions.find(
+          (productOption) => productOption.id === productOptionId,
+        );
+      },
     },
     ProductOptionGroup: {
       productGroup: (parent, args, context) => {
         const { productGroupId } = parent;
         return productGroups.find((productGroup) => productGroup.id === productGroupId);
+      },
+    },
+    ProductOption: {
+      productOptionGroup: (parent, args, context) => {
+        const { productOptionGroupId } = parent;
+        return productOptionGroups.find(
+          (productOptionGroup) => productOptionGroup.id === productOptionGroupId,
+        );
       },
     },
   },
