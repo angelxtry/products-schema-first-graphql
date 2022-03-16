1f8f55e7ac6fca4646d6c6189af93c286bd7a91f
sean <sean@gomicorp.com>
2022-03-16 14:04:35 +0900

    ProductOptionGroup 추가

--- index.js
+++ index.js
@@ -11,16 +11,35 @@ const productGroups = [
   },
 ];
 
+const productOptionGroups = [
+  {
+    id: 'pog1',
+    productGroupId: 'bbbb',
+    productOptionGroupName: '종류',
+  },
+  {
+    id: 'pog2',
+    productGroupId: 'bbbb',
+    productOptionGroupName: '용량',
+  },
+];
+
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
       hello: String!
       productGroups: [ProductGroup!]!
       productGroup(id: ID!): ProductGroup
+      productOptionGroups: [ProductOptionGroup!]!
     }
     type ProductGroup {
       id: ID!
       productGroupName: String!
+    }
+    type ProductOptionGroup {
+      id: ID!
+      productOptionGroupName: String!
+      productGroup: ProductGroup!
     }
   `,
   resolvers: {
@@ -28,6 +47,13 @@ const server = new ApolloServer({
       hello: () => 'world!',
       productGroups: () => productGroups,
       productGroup: (parent, { id: productGroupId }, context) => {
+        return productGroups.find((productGroup) => productGroup.id === productGroupId);
+      },
+      productOptionGroups: () => productOptionGroups,
+    },
+    ProductOptionGroup: {
+      productGroup: (parent, args, context) => {
+        const { productGroupId } = parent;
         return productGroups.find((productGroup) => productGroup.id === productGroupId);
       },
     },
